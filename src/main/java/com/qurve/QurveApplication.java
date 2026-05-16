51d2ced2a593c492da4533700af3fa0be184ece0
hyunji <hyunji_2005@naver.com>
2026-05-16 15:39:38 +0900

    chore : 프로젝트 초기 세팅

---
+++ src/main/java/com/qurve/QurveApplication.java
@@ -0,0 +1,13 @@
+package com.qurve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QurveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QurveApplication.class, args);
+    }
+
+}
