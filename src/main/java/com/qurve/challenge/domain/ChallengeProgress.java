fe5ea81de7c225c7e07ec4a69a4448c6c4d01b4e
sunjung <sunjeong2201@gmail.com>
2026-06-01 10:43:36 +0900

    feat: QVE-6-2-1 메인페이지 챌린지 조회 API 구현

---
+++ src/main/java/com/qurve/challenge/domain/ChallengeProgress.java
@@ -0,0 +1,33 @@
+package com.qurve.challenge.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tb_challenge_progress")
+public class ChallengeProgress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "progress_id")
+    private Long progressId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "challenge_id", nullable = false, unique = true)
+    private Challenge challenge;
+
+    @Builder.Default
+    @Column(name = "completed_days", nullable = false)
+    private Integer completedDays = 0;
+
+    @Builder.Default
+    @Column(name = "progress_rate", nullable = false)
+    private Integer progressRate = 0;
+}
