9adca96e2369055518e9354cb3c735ebe4d5f185
hyunji <hyunji_2005@naver.com>
2026-06-01 17:48:42 +0900

    feat : QVE-5-3-1 레벨 테스트 결과 API 구현

--- src/main/java/com/qurve/level/controller/LevelController.java
+++ src/main/java/com/qurve/level/controller/LevelController.java
@@ -2,7 +2,9 @@ package com.qurve.level.controller;
 
 import com.qurve.global.common.ApiResponse;
 import com.qurve.level.dto.request.LevelTestRequestDto;
+import com.qurve.level.dto.request.LevelTestResultRequestDto;
 import com.qurve.level.dto.response.LevelTestResponseDto;
+import com.qurve.level.dto.response.LevelTestResultResponseDto;
 import com.qurve.level.dto.response.PreQuestionResponseDto;
 import com.qurve.level.service.LevelService;
 import lombok.RequiredArgsConstructor;
@@ -25,5 +27,10 @@ public class LevelController {
     @PostMapping("/test")
     public ResponseEntity<ApiResponse<LevelTestResponseDto>> getLevelTestQuestions(@Validated @RequestBody LevelTestRequestDto levelTestRequestDto) {
         return ResponseEntity.ok(ApiResponse.success(levelService.getLevelTestQuestions(levelTestRequestDto)));
+    }
+
+    @PostMapping("/test/result")
+    public ResponseEntity<ApiResponse<LevelTestResultResponseDto>> levelTestResult(@Validated @RequestBody LevelTestResultRequestDto levelTestResultRequestDto) {
+        return ResponseEntity.ok(ApiResponse.success(levelService.levelTestResult(levelTestResultRequestDto)));
     }
 }
