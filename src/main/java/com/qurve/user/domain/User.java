ca4bf0a496bb75114bc6c281621eb53bd2b13358
hyunji <hyunji_2005@naver.com>
2026-08-08 10:25:21 +0900

    fix : QVE-1-2-1 loginId 길이 제한 255로 수정

--- src/main/java/com/qurve/user/domain/User.java
+++ src/main/java/com/qurve/user/domain/User.java
@@ -23,7 +23,7 @@ public class User extends BaseEntity {
     @Column(name = "user_id")
     private Long userId;
 
-    @Column(name = "login_id", length = 100, nullable = false, unique = true)
+    @Column(name = "login_id", length = 255, nullable = false, unique = true)
     private String loginId;
 
     @Column(name = "password_hash", length = 255, nullable = false)
